8d5bd99335f8ab11a260aac80c97557f22b61c9c
timtoronto634 <tim225x@gmail.com>
2021-06-13 20:04:43 +0900

    8.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -24,5 +25,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
         :password_confirmation)
-end
+    end
 end

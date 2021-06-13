bc1d61855099e406cce5f290872989b96a981935
timtoronto634 <tim225x@gmail.com>
2021-06-13 20:17:01 +0900

    8.3

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -11,6 +11,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     assert_template 'sessions/new'
     post login_path, params: {session: {email: "", password: ""}}
+    assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?

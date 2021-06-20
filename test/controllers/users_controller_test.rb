61f1fd3df5ce16af07e1fb500b2b64d72c54befa
timtoronto634 <tim225x@gmail.com>
2021-06-20 11:00:10 +0900

    10.4.1

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -31,6 +31,25 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  test "should change user naem" do
+    log_in_as(@user)
+    tmp_name = @user.name
+    patch user_path(@user), params: { user: {name: "hello"}}
+    # debugger
+    assert_not @user.reload.name == tmp_name
+  end
+  
+
+  test "should not allow the admin attribute to be edited via the web " do 
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: { user: { name: "hello",
+                                                    password: "password",
+                                                    password_confirmation: "password", 
+                                                    admin: true } }
+    assert_not @other_user.reload.admin?
+  end
+
   test "should redirect edit when logged in as another user" do
     log_in_as(@other_user)
     get edit_user_path(@user)
